0fdcaa71890fb52ba574b76fd2d1bc0ce6db8d69
Azz-9 <azz9lefaux@gmail.com>
2025-10-31 12:06:32 +0100

    la fonction drawBorder prend maintenant l'épaisseur de la bordure en paramètre

--- src/main/java/me/Azz_9/flex_hud/client/utils/DrawingUtils.java
+++ src/main/java/me/Azz_9/flex_hud/client/utils/DrawingUtils.java
@@ -3,10 +3,15 @@ package me.Azz_9.flex_hud.client.utils;
 import net.minecraft.client.gui.DrawContext;
 
 public class DrawingUtils {
+
 	public static void drawBorder(DrawContext context, int x, int y, int width, int height, int color) {
-		context.fill(x, y, x + width, y + 1, color);
-		context.fill(x, y + height - 1, x + width, y + height, color);
-		context.fill(x, y + 1, x + 1, y + height - 1, color);
-		context.fill(x + width - 1, y + 1, x + width, y + height - 1, color);
+		drawBorder(context, x, y, width, height, 1, color);
+	}
+
+	public static void drawBorder(DrawContext context, int x, int y, int width, int height, int thick, int color) {
+		context.fill(x, y, x + width, y + thick, color);
+		context.fill(x, y + height - thick, x + width, y + height, color);
+		context.fill(x, y + thick, x + thick, y + height - thick, color);
+		context.fill(x + width - thick, y + thick, x + width, y + height - thick, color);
 	}
 }
