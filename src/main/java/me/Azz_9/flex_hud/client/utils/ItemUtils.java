9dca38be9c205bb73cd097d0ee6a4c46ca8ffe69
Azz-9 <azz9lefaux@gmail.com>
2025-11-21 18:17:56 +0100

    les méthodes pour récupérer le nombre d'un item dans l'inventaire, prennent maintenant un Inventory et pas un PlayerEntity en paramètre

--- src/main/java/me/Azz_9/flex_hud/client/utils/ItemUtils.java
+++ src/main/java/me/Azz_9/flex_hud/client/utils/ItemUtils.java
@@ -1,7 +1,7 @@
 package me.Azz_9.flex_hud.client.utils;
 
 import net.minecraft.component.DataComponentTypes;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.Inventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -10,11 +10,11 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Objects;
 
 public class ItemUtils {
-	public static int getStackCount(@NotNull ItemStack stack, @NotNull PlayerEntity player) {
+	public static int getStackCount(@NotNull ItemStack stack, @NotNull Inventory inventory) {
 		int itemCount = 0;
 
-		for (int i = 0; i < player.getInventory().size(); ++i) {
-			ItemStack itemStack = player.getInventory().getStack(i);
+		for (int i = 0; i < inventory.size(); ++i) {
+			ItemStack itemStack = inventory.getStack(i);
 			if ((stack.isOf(Items.POTION) || stack.isOf(Items.SPLASH_POTION) || stack.isOf(Items.LINGERING_POTION) || stack.isOf(Items.TIPPED_ARROW))) {
 				if (itemStack.isOf(stack.getItem()) && Objects.equals(
 						itemStack.getComponents().get(DataComponentTypes.POTION_CONTENTS),
@@ -58,11 +58,11 @@ public class ItemUtils {
 		return itemCount;
 	}
 
-	public static int getItemCount(@NotNull Item item, @NotNull PlayerEntity player) {
+	public static int getItemCount(@NotNull Item item, @NotNull Inventory inventory) {
 		int itemCount = 0;
 
-		for (int i = 0; i < player.getInventory().size(); ++i) {
-			ItemStack itemStack = player.getInventory().getStack(i);
+		for (int i = 0; i < inventory.size(); ++i) {
+			ItemStack itemStack = inventory.getStack(i);
 			if (itemStack.isOf(item)) {
 				itemCount += itemStack.getCount();
 			}
