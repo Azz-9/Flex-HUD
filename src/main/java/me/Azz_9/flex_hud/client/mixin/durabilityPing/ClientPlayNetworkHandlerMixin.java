b9c2ff590145501920237389fe2896d0f6282c7c
Azz-9 <azz9lefaux@gmail.com>
2025-11-23 19:12:59 +0100

    changement du nom de la mixin sur ClientPlayNetworkHandler

--- src/main/java/me/Azz_9/flex_hud/client/mixin/durabilityPing/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/me/Azz_9/flex_hud/client/mixin/durabilityPing/ClientPlayNetworkHandlerMixin.java
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayNetworkHandler.class)
-public abstract class MixinClientPlayNetworkHandler {
+public abstract class ClientPlayNetworkHandlerMixin {
 
 	@Unique
 	private static long lastProcessedTick = -1;
