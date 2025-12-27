829ad6102b9c4f40cc09d0b05a6864605807b3e6
Azz-9 <azz9lefaux@gmail.com>
2025-12-27 15:09:55 +0100

    cliquer sur un cycling button avec shift enfoncé permet de revenir en arrière

--- src/main/java/me/Azz_9/flex_hud/client/screens/configurationScreen/configWidgets/buttons/ConfigCyclingButtonWidget.java
+++ src/main/java/me/Azz_9/flex_hud/client/screens/configurationScreen/configWidgets/buttons/ConfigCyclingButtonWidget.java
@@ -53,7 +53,9 @@ public class ConfigCyclingButtonWidget<T, E extends Enum<E> & Translatable> exte
 	public void onClick(Click click, boolean bl) {
 		super.onClick(click, bl);
 
-		int index = (variable.getValue().ordinal() + 1) % values.length;
+		// shift click to go backward
+		int offset = MinecraftClient.getInstance().isShiftPressed() ? -1 : 1;
+		int index = (variable.getValue().ordinal() + offset + values.length) % values.length;
 
 		setValue(values[index]);
 	}
