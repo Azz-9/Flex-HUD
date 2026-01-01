590c4fb8d0058d1caa492dfc18f68b8a2176b81b
Azz-9 <azz9lefaux@gmail.com>
2026-01-01 19:04:41 +0100

    smooth scroll désactivé quand un mod qui est fait pour en ajouter est installé (smooth scolling ou smooth scrolling refurbish)

--- src/main/java/me/Azz_9/flex_hud/client/screens/AbstractSmoothScrollableList.java
+++ src/main/java/me/Azz_9/flex_hud/client/screens/AbstractSmoothScrollableList.java
@@ -1,24 +1,30 @@
 package me.Azz_9.flex_hud.client.screens;
 
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.ContainerObjectSelectionList;
 import org.jspecify.annotations.NonNull;
 
 public abstract class AbstractSmoothScrollableList<E extends ContainerObjectSelectionList.Entry<E>> extends ContainerObjectSelectionList<E> {
-	private double targetScroll;     // Target scroll amount (set by mouse wheel)
-	private double currentScroll;    // Interpolated scroll amount (used for rendering)
+	private double targetScroll = 0; // Target scroll amount (set by mouse wheel)
+	private double currentScroll = 0; // Interpolated scroll amount (used for rendering)
 	private final double SCROLL_SPEED = 25.0; // Pixels per notch
 	private long lastUpdateTime = System.nanoTime();
 
+	private final boolean externalSmoothDetected = FabricLoader.getInstance().isModLoaded("smoothscroll") ||
+			FabricLoader.getInstance().isModLoaded("smoothscrollingrefurbished");
+
 	public AbstractSmoothScrollableList(Minecraft minecraftClient, int width, int height, int y, int itemHeight) {
 		super(minecraftClient, width, height, y, itemHeight);
-		this.targetScroll = 0.0;
-		this.currentScroll = 0.0;
 	}
 
 	@Override
 	public boolean mouseScrolled(double mouseX, double mouseY, double horizontalAmount, double verticalAmount) {
+		if (externalSmoothDetected) {
+			return super.mouseScrolled(mouseX, mouseY, horizontalAmount, verticalAmount);
+		}
+
 		// Update the target scroll position
 		targetScroll -= verticalAmount * SCROLL_SPEED;
 		targetScroll = Math.clamp(targetScroll, 0.0F, this.maxScrollAmount() + 1);
@@ -26,7 +32,12 @@ public abstract class AbstractSmoothScrollableList<E extends ContainerObjectSele
 	}
 
 	@Override
-	protected void renderListItems(@NonNull GuiGraphics graphics, int mouseX, int mouseY, float a) {
+	protected void renderListItems(@NonNull GuiGraphics graphics, int mouseX, int mouseY, float delta) {
+		if (externalSmoothDetected) {
+			super.renderListItems(graphics, mouseX, mouseY, delta);
+			return;
+		}
+
 		long currentTime = System.nanoTime();
 		double deltaSeconds = (currentTime - lastUpdateTime) / 1_000_000_000.0; // Convertir en secondes
 		lastUpdateTime = currentTime;
@@ -36,15 +47,18 @@ public abstract class AbstractSmoothScrollableList<E extends ContainerObjectSele
 		currentScroll += (targetScroll - currentScroll) * alpha;
 
 		super.setScrollAmount(currentScroll);
-		super.render(graphics, mouseX, mouseY, a);
+		super.render(graphics, mouseX, mouseY, delta);
 	}
 
 	@Override
 	public void setScrollAmount(double scrollY) {
+		if (externalSmoothDetected) {
+			super.setScrollAmount(scrollY);
+			return;
+		}
+
 		super.setScrollAmount(scrollY);
 		targetScroll = scrollY;
 		currentScroll = scrollY;
 	}
-
-
 }
