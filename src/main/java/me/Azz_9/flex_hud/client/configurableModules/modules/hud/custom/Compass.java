832db24c146df4dbb68e8ad26e9fdcc36f3f3e90
Azz-9 <azz9lefaux@gmail.com>
2026-01-09 08:33:10 +0100

    début pitch display

--- src/main/java/me/Azz_9/flex_hud/client/configurableModules/modules/hud/custom/Compass.java
+++ src/main/java/me/Azz_9/flex_hud/client/configurableModules/modules/hud/custom/Compass.java
@@ -567,6 +567,13 @@ public class Compass extends AbstractTextModule {
 	}
 
 	@Override
+	public List<String> getKeywords() {
+		List<String> keywords = new ArrayList<>(super.getKeywords());
+		keywords.add("yaw");
+		return keywords;
+	}
+
+	@Override
 	public AbstractConfigurationScreen getConfigScreen(Screen parent) {
 		return new AbstractConfigurationScreen(getName(), parent) {
 			@Override
