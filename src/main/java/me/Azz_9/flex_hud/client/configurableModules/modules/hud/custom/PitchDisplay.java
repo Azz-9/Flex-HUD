4ee1a25c21591fd55d662f9b570b0ade31d54212
Azz-9 <azz9lefaux@gmail.com>
2026-01-10 22:27:15 +0100

    module pitch display terminé

--- src/main/java/me/Azz_9/flex_hud/client/configurableModules/modules/hud/custom/PitchDisplay.java
+++ src/main/java/me/Azz_9/flex_hud/client/configurableModules/modules/hud/custom/PitchDisplay.java
@@ -5,8 +5,10 @@ import me.Azz_9.flex_hud.client.configurableModules.ConfigRegistry;
 import me.Azz_9.flex_hud.client.configurableModules.modules.hud.AbstractTextModule;
 import me.Azz_9.flex_hud.client.screens.configurationScreen.AbstractConfigurationScreen;
 import me.Azz_9.flex_hud.client.screens.configurationScreen.configEntries.ColorButtonEntry;
+import me.Azz_9.flex_hud.client.screens.configurationScreen.configEntries.IntFieldEntry;
 import me.Azz_9.flex_hud.client.screens.configurationScreen.configEntries.ToggleButtonEntry;
 import me.Azz_9.flex_hud.client.screens.configurationScreen.configVariables.ConfigBoolean;
+import me.Azz_9.flex_hud.client.screens.configurationScreen.configVariables.ConfigInteger;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
@@ -20,7 +22,10 @@ import org.joml.Matrix3x2fStack;
 
 public class PitchDisplay extends AbstractTextModule {
 
-	private final ConfigBoolean displayWhenElytraIsEquipped = new ConfigBoolean(false, ""); //TODO trad
+	private final ConfigBoolean displayWhenElytraIsEquipped = new ConfigBoolean(false, "flex_hud.pitch_display.config.display_when_elytra_equipped");
+	private final ConfigBoolean showMarker = new ConfigBoolean(true, "flex_hud.pitch_display.config.show_marker");
+	private final ConfigBoolean showDegrees = new ConfigBoolean(false, "flex_hud.pitch_display.config.show_degrees");
+	private final ConfigInteger degreesDecimals = new ConfigInteger(0, "flex_hud.pitch_display.config.degrees_decimals", 0, 14);
 
 	public PitchDisplay(double defaultOffsetX, double defaultOffsetY, @NotNull AnchorPosition defaultAnchorX, @NotNull AnchorPosition defaultAnchorY) {
 		super(defaultOffsetX, defaultOffsetY, defaultAnchorX, defaultAnchorY);
@@ -29,9 +34,11 @@ public class PitchDisplay extends AbstractTextModule {
 		this.enabled.setConfigTextTranslationKey("flex_hud.pitch_display.config.enable");
 
 		ConfigRegistry.register(getID(), "displayWhenElytraIsEquipped", displayWhenElytraIsEquipped);
+		ConfigRegistry.register(getID(), "showMarker", showMarker);
+		ConfigRegistry.register(getID(), "showDegrees", showDegrees);
+		ConfigRegistry.register(getID(), "degreesDecimals", degreesDecimals);
 
-		setHeight(210);
-		setWidth(30);
+		setHeight(150);
 	}
 
 	@Override
@@ -49,17 +56,34 @@ public class PitchDisplay extends AbstractTextModule {
 		MinecraftClient client = MinecraftClient.getInstance();
 		PlayerEntity player = client.player;
 
+		String format = "%." + degreesDecimals.getValue() + "f";
 		if (shouldNotRender() || !Flex_hudClient.isInMoveElementScreen && player == null) {
 			return;
 		}
 
+
+		String pitchStr;
 		float pitch;
 		if (Flex_hudClient.isInMoveElementScreen) {
-			pitch = 180;
+			pitch = 0;
+			pitchStr = String.format(format, pitch);
 		} else {
 			// Calcul de la direction (pitch)
 			pitch = (player.getPitch() % 360 + 360) % 360;
+			pitchStr = String.format(format, -player.getPitch());
 		}
+
+		float degreesScale = 0.75f;
+
+		String markerText = "▶";
+
+		setWidth((int) ((showDegrees.getValue()
+				? client.textRenderer.getWidth(pitchStr) * degreesScale + 2
+				: 0) +
+				(showMarker.getValue()
+						? (client.textRenderer.getWidth(markerText) / 2.0 + 5)
+						: 0) + 34));
+
 
 		Matrix3x2fStack matrices = context.getMatrices();
 		matrices.pushMatrix();
@@ -68,9 +92,38 @@ public class PitchDisplay extends AbstractTextModule {
 
 		drawBackground(context);
 
-		for (int angle = 0; angle < 360; angle += 45) {
-			drawPitchPoint(context, matrices, angle, pitch);
+		context.enableScissor(0, 0, getWidth(), getHeight());
+
+		float hudX = 0;
+		if (showDegrees.getValue()) {
+			matrices.pushMatrix();
+			matrices.translate(hudX, (getHeight() - client.textRenderer.fontHeight) / 2.0f);
+			matrices.scale(degreesScale);
+			context.drawText(client.textRenderer, pitchStr, 0, 0, getColor(), shadow.getValue());
+			matrices.popMatrix();
+
+			hudX += client.textRenderer.getWidth(pitchStr) * degreesScale + 2;
 		}
+
+		if (this.showMarker.getValue()) {
+			matrices.pushMatrix();
+			matrices.translate(hudX, (getHeight() - client.textRenderer.fontHeight) / 2.0f);
+			matrices.scale(0.5f, 1.0f);
+			context.drawText(client.textRenderer, markerText, 0, 0, getColor(), this.shadow.getValue());
+			matrices.popMatrix();
+
+			hudX += client.textRenderer.getWidth(markerText) / 2.0f + 5;
+		}
+
+		drawIntermediatePoint(context, matrices, -165, pitch, hudX);
+		drawIntermediatePoint(context, matrices, -150, pitch, hudX);
+		for (int angle = -135; angle <= 135; angle += 45) {
+			drawPitchPoint(context, matrices, angle, pitch, hudX);
+			drawIntermediatePoint(context, matrices, angle + 15, pitch, hudX);
+			drawIntermediatePoint(context, matrices, angle + 30, pitch, hudX);
+		}
+
+		context.disableScissor();
 
 		matrices.popMatrix();
 	}
@@ -81,27 +134,57 @@ public class PitchDisplay extends AbstractTextModule {
 		return super.shouldNotRender() || player != null && displayWhenElytraIsEquipped.getValue() && !player.getInventory().getStack(38).isOf(Items.ELYTRA);
 	}
 
-	private void drawPitchPoint(DrawContext drawContext, Matrix3x2fStack matrices, int angle, float pitch) {
+	private void drawPitchPoint(DrawContext context, Matrix3x2fStack matrices, int angle, float pitch, float x) {
 		MinecraftClient client = MinecraftClient.getInstance();
 		String label = "|";
+		String angleStr = String.valueOf(angle);
+		angle = -angle;
+
+		float angleDifference = (angle - pitch + 540) % 360 - 180;
+
+		float scaleFactor = 1.25f;
+		float angleScale = 0.75f;
+
+		if (Math.abs(angleDifference) <= 120) {
+			// Calculer la position Y de chaque point cardinal en fonction de l'angle
+			float positionY = ((getHeight() / 2.0f) + (angleDifference * (getHeight() / 180.0f)));
+			float pointWidth = client.textRenderer.getWidth(label) * scaleFactor;
+			float angleHeight = client.textRenderer.fontHeight * angleScale;
+
+			matrices.pushMatrix();
+			matrices.translate(x + 14, positionY - angleHeight / 2.0f);
+			matrices.scale(angleScale, angleScale);
+			context.drawText(client.textRenderer, angleStr, 0, 0, getColorWithFadeEffect(positionY), shadow.getValue());
+			matrices.popMatrix();
+
+			matrices.pushMatrix();
+			matrices.translate(x + 9, positionY - pointWidth / 2.0f);
+			matrices.scale(scaleFactor, scaleFactor);
+			matrices.rotate((float) Math.toRadians(90));
+			context.drawText(client.textRenderer, label, 0, 0, getColorWithFadeEffect(positionY), shadow.getValue());
+			matrices.popMatrix();
+		}
+	}
+
+	private void drawIntermediatePoint(DrawContext context, Matrix3x2fStack matrices, int angle, float pitch, float x) {
+		MinecraftClient client = MinecraftClient.getInstance();
+		String label = "|";
+		angle = -angle;
 
 		float angleDifference = (angle - pitch + 540) % 360 - 180;
 
 		if (Math.abs(angleDifference) <= 120) {
-			float scaleFactor = 1.25f;
+			float scaleFactor = 0.75f;
 			// Calculer la position Y de chaque point cardinal en fonction de l'angle
 			float positionY = ((getHeight() / 2.0f) + (angleDifference * (getHeight() / 180.0f)));
 			float pointWidth = client.textRenderer.getWidth(label) * scaleFactor;
 
-			// Afficher le label des directions avec couleur et taille de texte ajustée
 			matrices.pushMatrix();
-			matrices.translate(8, positionY - pointWidth / 2.0f);
+			matrices.translate(x + 5.6f, positionY - pointWidth / 2.0f);
 			matrices.scale(scaleFactor, scaleFactor);
 			matrices.rotate((float) Math.toRadians(90));
-			drawContext.drawText(client.textRenderer, label, 0, 0, getColorWithFadeEffect(positionY), this.shadow.getValue());
+			context.drawText(client.textRenderer, label, 0, 0, getColorWithFadeEffect(positionY), shadow.getValue());
 			matrices.popMatrix();
-
-
 		}
 	}
 
@@ -123,8 +206,15 @@ public class PitchDisplay extends AbstractTextModule {
 	@Override
 	public AbstractConfigurationScreen getConfigScreen(Screen parent) {
 		return new AbstractConfigurationScreen(getName(), parent) {
+
 			@Override
 			protected void init() {
+				if (MinecraftClient.getInstance().getLanguageManager().getLanguage().equals("fr_fr")) {
+					buttonWidth = 250;
+				} else {
+					buttonWidth = 190;
+				}
+
 				super.init();
 
 				this.addAllEntries(
@@ -174,6 +264,24 @@ public class PitchDisplay extends AbstractTextModule {
 								.setToggleButtonWidth(buttonWidth)
 								.setVariable(displayWhenElytraIsEquipped)
 								.addDependency(this.getConfigList().getFirstEntry(), false)
+								.build(),
+						new ToggleButtonEntry.Builder()
+								.setToggleButtonWidth(buttonWidth)
+								.setVariable(showMarker)
+								.addDependency(this.getConfigList().getFirstEntry(), false)
+								.build(),
+						new ToggleButtonEntry.Builder()
+								.setToggleButtonWidth(buttonWidth)
+								.setVariable(showDegrees)
+								.addDependency(this.getConfigList().getFirstEntry(), false)
+								.build()
+				);
+				this.addAllEntries(
+						new IntFieldEntry.Builder()
+								.setIntFieldWidth(20)
+								.setVariable(degreesDecimals)
+								.addDependency(this.getConfigList().getFirstEntry(), false)
+								.addDependency(this.getConfigList().getLastEntry(), false)
 								.build()
 				);
 			}
