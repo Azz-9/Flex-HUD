a6e1934e883ff7cdc0bdde12646cc2cfe2190d53
Azz-9 <azz9lefaux@gmail.com>
2026-01-31 13:20:58 +0100

    Revert "utilisation du ping logger déjà implémenté pour calculer l'average ping"
    This reverts commit cc01c5fedc655c9d0554d1221780d36f530a15e5.

--- src/main/java/me/Azz_9/flex_hud/client/configurableModules/modules/hud/custom/Ping.java
+++ src/main/java/me/Azz_9/flex_hud/client/configurableModules/modules/hud/custom/Ping.java
@@ -2,7 +2,6 @@ package me.Azz_9.flex_hud.client.configurableModules.modules.hud.custom;
 
 import me.Azz_9.flex_hud.client.Flex_hudClient;
 import me.Azz_9.flex_hud.client.configurableModules.ConfigRegistry;
-import me.Azz_9.flex_hud.client.configurableModules.modules.TickableModule;
 import me.Azz_9.flex_hud.client.configurableModules.modules.hud.AbstractTextModule;
 import me.Azz_9.flex_hud.client.screens.configurationScreen.AbstractConfigurationScreen;
 import me.Azz_9.flex_hud.client.screens.configurationScreen.configEntries.ColorButtonEntry;
@@ -11,15 +10,14 @@ import me.Azz_9.flex_hud.client.screens.configurationScreen.configVariables.Conf
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.text.Text;
-import net.minecraft.util.profiler.MultiValueDebugSampleLogImpl;
 import org.jetbrains.annotations.NotNull;
 import org.joml.Matrix3x2fStack;
 
-public class Ping extends AbstractTextModule implements TickableModule {
+public class Ping extends AbstractTextModule {
 	private final ConfigBoolean hideWhenOffline = new ConfigBoolean(true, "flex_hud.ping.config.hide_when_offline");
-	private long averagePing = 0;
 
 	public Ping(double defaultOffsetX, double defaultOffsetY, @NotNull AnchorPosition defaultAnchorX, @NotNull AnchorPosition defaultAnchorY) {
 		super(defaultOffsetX, defaultOffsetY, defaultAnchorX, defaultAnchorY);
@@ -62,7 +60,15 @@ public class Ping extends AbstractTextModule implements TickableModule {
 		} else {
 			if (client.getCurrentServerEntry() != null) {
 
-				text = averagePing + " ms";
+				if (client.getNetworkHandler() != null) {
+					PlayerListEntry entry = client.getNetworkHandler().getPlayerListEntry(client.player.getUuid());
+
+					if (entry != null) {
+						int latency = entry.getLatency();
+
+						text = latency + " ms";
+					}
+				}
 
 			} else if (!this.hideWhenOffline.getValue()) {
 
@@ -155,19 +161,5 @@ public class Ping extends AbstractTextModule implements TickableModule {
 				);
 			}
 		};
-	}
-
-	@Override
-	public void tick() {
-		MultiValueDebugSampleLogImpl pingLogger = MinecraftClient.getInstance().getDebugHud().getPingLog();
-
-		if (pingLogger.getLength() > 0) {
-			long total = 0;
-			for (int i = 0; i < pingLogger.getLength(); i++) {
-				total += pingLogger.get(i);
-			}
-
-			averagePing = total / pingLogger.getLength();
-		}
 	}
 }
