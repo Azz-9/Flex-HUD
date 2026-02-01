7caf7d3eb7f59f2e28152ea3c78bffa16e06fd5b
Azz-9 <azz9lefaux@gmail.com>
2026-02-01 13:33:09 +0100

    nouvelle version 0.5.2 et peut-être fix du ping sur les serveurs bungee

--- src/main/java/me/Azz_9/flex_hud/client/mixin/ping/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/me/Azz_9/flex_hud/client/mixin/ping/ClientPlayNetworkHandlerMixin.java
@@ -2,6 +2,7 @@ package me.Azz_9.flex_hud.client.mixin.ping;
 
 import me.Azz_9.flex_hud.client.configurableModules.modules.hud.custom.Ping;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.packet.s2c.play.EnterReconfigurationS2CPacket;
 import net.minecraft.network.packet.s2c.query.PingResultS2CPacket;
 import net.minecraft.util.Util;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,9 +11,16 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayNetworkHandler.class)
-public class ClientPlayNetworkHandlerMixin {
+public abstract class ClientPlayNetworkHandlerMixin {
+
 	@Inject(method = "onPingResult", at = @At("HEAD"))
 	private void onPingResult(PingResultS2CPacket packet, CallbackInfo ci) {
 		Ping.ping = Util.getMeasuringTimeMs() - packet.startTime();
+	}
+
+	@Inject(method = "onEnterReconfiguration", at = @At("HEAD"))
+	private void onEnterReconfiguration(EnterReconfigurationS2CPacket packet, CallbackInfo ci) {
+		Ping.stopPinging();
+		Ping.packetSender = null;
 	}
 }
