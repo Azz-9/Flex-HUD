e089b2ec2a11604716bc12ddebad7c53814b7a43
Azz-9 <azz9lefaux@gmail.com>
2026-02-06 19:52:14 +0100

    ajout des modules toggleSprint et toggleSneak

---
+++ src/main/java/me/Azz_9/flex_hud/client/configurableModules/modules/hud/custom/ToggleSneak.java
@@ -0,0 +1,145 @@
+package me.Azz_9.flex_hud.client.configurableModules.modules.hud.custom;
+
+import static me.Azz_9.flex_hud.client.Flex_hudClient.CLIENT;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.render.RenderTickCounter;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.joml.Matrix3x2fStack;
+
+import me.Azz_9.flex_hud.client.Flex_hudClient;
+import me.Azz_9.flex_hud.client.configurableModules.modules.hud.AbstractTextModule;
+import me.Azz_9.flex_hud.client.mixin.toggleSprintSneak.KeyBindingAccessor;
+import me.Azz_9.flex_hud.client.screens.configurationScreen.AbstractConfigurationScreen;
+import me.Azz_9.flex_hud.client.screens.configurationScreen.configEntries.ColorButtonEntry;
+import me.Azz_9.flex_hud.client.screens.configurationScreen.configEntries.ToggleButtonEntry;
+
+public class ToggleSneak extends AbstractTextModule {
+
+	public ToggleSneak(double defaultOffsetX, double defaultOffsetY, @NotNull AnchorPosition defaultAnchorX, @NotNull AnchorPosition defaultAnchorY) {
+		super(defaultOffsetX, defaultOffsetY, defaultAnchorX, defaultAnchorY);
+		this.enabled.setValue(false);
+		this.enabled.setDefaultValue(false);
+		this.enabled.setConfigTextTranslationKey("flex_hud.toggle_sneak.config.enable");
+	}
+
+	@Override
+	public void init() {
+		this.setHeight(CLIENT.textRenderer.fontHeight);
+	}
+
+	@Override
+	public Text getName() {
+		return Text.translatable("flex_hud.toggle_sneak");
+	}
+
+	@Override
+	public String getID() {
+		return "toggle_sneak";
+	}
+
+	@Override
+	public void render(DrawContext context, RenderTickCounter tickCounter) {
+		if (shouldNotRender() || CLIENT.player == null && !Flex_hudClient.isInMoveElementScreen) {
+			return;
+		}
+
+		String statusMessage = getStatusMessage(CLIENT.player);
+
+		setWidth(statusMessage);
+
+		Matrix3x2fStack matrices = context.getMatrices();
+		matrices.pushMatrix();
+		matrices.translate(getRoundedX(), getRoundedY());
+		matrices.scale(getScale());
+
+		drawBackground(context);
+
+		context.drawText(CLIENT.textRenderer, statusMessage, 0, 0, getColor(), this.shadow.getValue());
+
+		matrices.popMatrix();
+	}
+
+	private static @NotNull String getStatusMessage(@Nullable ClientPlayerEntity player) {
+		String statusMessage = "";
+		if (Flex_hudClient.isInMoveElementScreen || player == null) {
+			statusMessage = "Sneaking (Toggled)";
+		} else {
+			boolean sneakKeyPressed = CLIENT.options.sneakKey.isPressed();
+			boolean sneakToggled = CLIENT.options.getSneakToggled().getValue();
+
+			if (sneakToggled) {
+				if (InputUtil.isKeyPressed(CLIENT.getWindow(), ((KeyBindingAccessor) CLIENT.options.sneakKey).getBoundKey().getCode())) {
+					statusMessage = "Sneaking (Held)";
+				} else if (sneakKeyPressed) {
+					statusMessage = "Sneaking (Toggled)";
+				}
+			} else if (sneakKeyPressed) {
+				statusMessage = "Sneaking (Held)";
+			}
+		}
+
+		return statusMessage;
+	}
+
+	@Override
+	public AbstractConfigurationScreen getConfigScreen(Screen parent) {
+		return new AbstractConfigurationScreen(getName(), parent) {
+			@Override
+			protected void init() {
+				super.init();
+
+				this.addAllEntries(
+						new ToggleButtonEntry.Builder()
+								.setToggleButtonWidth(buttonWidth)
+								.setVariable(enabled)
+								.build()
+				);
+				this.addAllEntries(
+						new ToggleButtonEntry.Builder()
+								.setToggleButtonWidth(buttonWidth)
+								.setVariable(shadow)
+								.addDependency(this.getConfigList().getFirstEntry(), false)
+								.build(),
+						new ToggleButtonEntry.Builder()
+								.setToggleButtonWidth(buttonWidth)
+								.setVariable(chromaColor)
+								.addDependency(this.getConfigList().getFirstEntry(), false)
+								.build()
+				);
+				this.addAllEntries(
+						new ColorButtonEntry.Builder()
+								.setColorButtonWidth(buttonWidth)
+								.setVariable(color)
+								.addDependency(this.getConfigList().getFirstEntry(), false)
+								.addDependency(this.getConfigList().getLastEntry(), true)
+								.build(),
+						new ToggleButtonEntry.Builder()
+								.setToggleButtonWidth(buttonWidth)
+								.setVariable(drawBackground)
+								.addDependency(this.getConfigList().getFirstEntry(), false)
+								.build()
+				);
+				this.addAllEntries(
+						new ColorButtonEntry.Builder()
+								.setColorButtonWidth(buttonWidth)
+								.setVariable(backgroundColor)
+								.addDependency(this.getConfigList().getFirstEntry(), false)
+								.addDependency(this.getConfigList().getLastEntry(), false)
+								.build(),
+						new ToggleButtonEntry.Builder()
+								.setToggleButtonWidth(buttonWidth)
+								.setVariable(hideInF3)
+								.addDependency(this.getConfigList().getFirstEntry(), false)
+								.build()
+				);
+			}
+		};
+	}
+}
