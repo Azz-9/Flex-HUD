2e9902b3ea31e20d13e9e6297ddfcc0c3f37fad5
Azz-9 <azz9lefaux@gmail.com>
2026-07-16 18:50:27 +0200

    code en commun dans XaeroWaypointCollector et JourneyMapWaypointCollector mis dans Collector

--- common/src/main/java/me/Azz_9/flex_hud/compat/waypointsCollectors/Collector.java
+++ common/src/main/java/me/Azz_9/flex_hud/compat/waypointsCollectors/Collector.java
@@ -1,16 +1,23 @@
 package me.Azz_9.flex_hud.compat.waypointsCollectors;
 
+import net.minecraft.client.Minecraft;
+
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import me.Azz_9.flex_hud.client.DimensionTracker;
 import me.Azz_9.flex_hud.client.modules.hud.custom.Compass;
+import me.Azz_9.flex_hud.client.tickables.Tickable;
 
-public abstract class Collector<Waypoint extends Compass.ModdedWaypoint> {
+public abstract class Collector<Waypoint extends Compass.ModdedWaypoint> implements Tickable {
 	private final @NotNull List<@NotNull Waypoint> waypoints = new ArrayList<>();
 
+	public boolean available = false;
 	private boolean joinedWorld;
+
+	public abstract void init();
 
 	public abstract void initCompassList();
 
@@ -30,5 +37,16 @@ public abstract class Collector<Waypoint extends Compass.ModdedWaypoint> {
 
 	public @NotNull List<@NotNull Waypoint> getWaypoints() {
 		return waypoints;
+	}
+
+	@Override
+	public void tick(Minecraft minecraft) {
+		if ((isJoinedWorld() && !available) || DimensionTracker.shouldInit) {
+			init();
+		} else {
+			DimensionTracker.check();
+		}
+
+		updateWaypoints();
 	}
 }
