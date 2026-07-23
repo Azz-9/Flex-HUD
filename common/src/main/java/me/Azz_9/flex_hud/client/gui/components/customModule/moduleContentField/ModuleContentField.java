f13602aa7f1a7b716f49f72644ddbf3eeeb2e56b
Azz-9 <azz9lefaux@gmail.com>
2026-07-23 20:01:16 +0200

    accès à scissorStack n'est plus délégué à Fabric et Neoforge

--- common/src/main/java/me/Azz_9/flex_hud/client/gui/components/customModule/moduleContentField/ModuleContentField.java
+++ common/src/main/java/me/Azz_9/flex_hud/client/gui/components/customModule/moduleContentField/ModuleContentField.java
@@ -33,7 +33,6 @@ import me.Azz_9.flex_hud.client.modules.customModules.modifiers.Modifier;
 import me.Azz_9.flex_hud.client.modules.customModules.modifiers.Modifiers;
 import me.Azz_9.flex_hud.client.modules.customModules.text.CustomCondition;
 import me.Azz_9.flex_hud.client.tickables.ChromaColorTickable;
-import me.Azz_9.flex_hud.platform.Services;
 import me.Azz_9.flex_hud.utils.DrawingUtils;
 
 public class ModuleContentField extends AbstractWidget implements TrackableChange {
@@ -281,7 +280,7 @@ public class ModuleContentField extends AbstractWidget implements TrackableChang
 			}
 		}
 
-		ScreenRectangle currentScissor = Services.PLATFORM.scissorStackPeek(graphics);
+		ScreenRectangle currentScissor = graphics.scissorStack.peek();
 		if (currentScissor != null) {
 			graphics.disableScissor();
 		}
