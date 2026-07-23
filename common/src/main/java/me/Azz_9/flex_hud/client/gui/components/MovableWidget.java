acb9b388e9417ba92003448b87b1fce07a98dbbf
Azz-9 <azz9lefaux@gmail.com>
2026-07-23 11:51:48 +0200

    certaines méthodes et valeurs pas utilisée supprimées

--- common/src/main/java/me/Azz_9/flex_hud/client/gui/components/MovableWidget.java
+++ common/src/main/java/me/Azz_9/flex_hud/client/gui/components/MovableWidget.java
@@ -89,13 +89,6 @@ public class MovableWidget extends AbstractWidget implements TrackableChange {
 		updateScaleHandle();
 	}
 
-	// i don't want to use the render method that already exists in ClickableWidget because it sets the value of hovered, and here, i'm setting this in the method mouseMove
-	public void draw(GuiGraphics graphics, int mouseX, int mouseY, float deltaTicks) {
-		if (this.visible) {
-			this.renderWidget(graphics, mouseX, mouseY, deltaTicks);
-		}
-	}
-
 	@Override
 	protected void renderWidget(@NotNull GuiGraphics graphics, int mouseX, int mouseY, float deltaTicks) {
 		this.isHovered = (mouseX >= getX() && mouseY >= getY() && mouseX <= getRight() && mouseY <= getBottom()) || isScaleHandleHovered(mouseX, mouseY);
