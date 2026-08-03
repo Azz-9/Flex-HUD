9211e761f0916c2c328ec80f4124b236780470b0
Azz-9 <azz9lefaux@gmail.com>
2026-08-03 23:10:18 +0200

    ajout icone pour resource pack et tps nouvelles variables tps et resource pack nouvelle option dynamic color pour le ping et pour tps nouvelle option show description pour le module resource pack

--- common/src/main/java/me/Azz_9/flex_hud/CommonClass.java
+++ common/src/main/java/me/Azz_9/flex_hud/CommonClass.java
@@ -35,6 +35,7 @@ import me.Azz_9.flex_hud.compat.waypointsCollectors.XaeroWaypointCollector;
 import me.Azz_9.flex_hud.platform.Services;
 import me.Azz_9.flex_hud.utils.FaviconUtils;
 import me.Azz_9.flex_hud.utils.PingUtils;
+import me.Azz_9.flex_hud.utils.TpsUtils;
 
 // This class is part of the common project meaning it is shared between all supported loaders. Code written here can only
 // import and access the vanilla codebase, libraries used by vanilla, and optionally third party libraries that provide
@@ -136,6 +137,7 @@ public class CommonClass {
 		Services.PLATFORM.registerDisconnectEvent(() -> {
 			PingUtils.stopPinging();
 			PingUtils.connection = null;
+			TpsUtils.reset();
 			waypointCollectors.forEach(Collector::onLeaveWorld);
 		});
 
