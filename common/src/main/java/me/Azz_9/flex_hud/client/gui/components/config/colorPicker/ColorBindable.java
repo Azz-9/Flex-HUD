d1816b77e25bd3d4277e8d14cf4a099c352af27e
Azz-9 <azz9lefaux@gmail.com>
2026-08-27 17:34:00 +0200

    color selector -> color picker gradient et hue bar sont maintenant dessinés avec des shaders

--- common/src/main/java/me/Azz_9/flex_hud/client/gui/components/config/colorPicker/ColorBindable.java
+++ common/src/main/java/me/Azz_9/flex_hud/client/gui/components/config/colorPicker/ColorBindable.java
@@ -1,4 +1,4 @@
-package me.Azz_9.flex_hud.client.gui.components.config.colorSelector;
+package me.Azz_9.flex_hud.client.gui.components.config.colorPicker;
 
 public interface ColorBindable {
 	void onReceiveColor(int color);
